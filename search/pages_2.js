4ab17232ea7d6bcfa4ccaf2ce2a46f790530b331
alxbilger <alxbilger@users.noreply.github.com>
2025-01-15 16:06:39 +0000

    Deploying to gh-pages from  @ fc0fc0590a456ce5bfa769c47dc94b6dc3205cfd 🚀

--- search/pages_2.js
+++ search/pages_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['theory_450',['theory',['../md_doc_modeling_theory.html',1,'']]],
-  ['todo_20list_451',['Todo List',['../todo.html',1,'']]]
+  ['documentation_0',['Beam Adapter Documentation',['../index.html',1,'']]]
 ];
